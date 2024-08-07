5d1aff265de6ca635a3907b08a59a90c79378776
estefaniamsp <estefania.sanchez01@epn.edu.ec>
2024-08-07 00:59:31 -0500

    cors

--- src/config/mensajeria.js
+++ src/config/mensajeria.js
@@ -2,11 +2,7 @@ import Chat from "../models/chat.js";
 import { Server } from "socket.io";
 import server from "../server.js";
 
-const io = new Server(server, {
-	cors: {
-		origin: "*",
-	},
-});
+const io = new Server(server);
 
 export default () => {
 	io.on("connection", (socket) => {
